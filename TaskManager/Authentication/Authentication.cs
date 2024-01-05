ce5936c93fff538d04d0b5f2abe9affed66c1cec
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-05 10:58:02 -0300

    remove while true

--- TaskManager/Authentication/Authentication.cs
+++ TaskManager/Authentication/Authentication.cs
@@ -12,34 +12,26 @@ namespace TaskManager.Authentication
     {
         public static User? PerformAuthentication(List<User> users)
         {
-            while(true)
-            {
-                Console.Clear();
-                Console.Write("Digite o username: ");
-                string username = Console.ReadLine();
-                Console.Write("Digite a senha: ");
-                string password = Console.ReadLine();
-                var user = users.Find(f => f.Username == username);
-                if (ValidateUser(user, password))
-                {
-                    return user;
-                    break;
-                }
-            }
+            Console.Clear();
+            Console.Write("Digite o username: ");
+            string username = Console.ReadLine();
+            Console.Write("Digite a senha: ");
+            string password = Console.ReadLine();
+            var user = users.Find(f => f.Username == username);
+            ValidateUser(user, password);
+            return user;
         }
 
-        private static bool ValidateUser(User? user, string password)
+        private static void ValidateUser(User? user, string password)
         {
             if (user != null && user.Password == password)
             {
                 user.Greet();
-                return true;
             }
             else
             {
                 Console.WriteLine("Username e/ou senha Incorretos. Tente novamente!");
                 Menu.WaitInput();
-                return false;
             }
         }
     }
